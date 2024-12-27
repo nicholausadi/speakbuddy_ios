7d266948159993a217acbd19d343e68eab778ee1
Nicholaus Adisetyo Purnomo <nicho.purnomo@gmail.com>
2024-12-27 13:00:57 +0700

    feat: Initial commit
    a

---
+++ speakbuddy-ios/speakbuddy_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  speakbuddy_iosApp.swift
+//  speakbuddy-ios
+//
+//  Created by Nicholaus Adisetyo Purnomo on 27/12/24.
+//
+
+import SwiftUI
+
+@main
+struct speakbuddy_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
